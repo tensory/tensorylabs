3884de1a2c30a4b635e33872cfa6d2dd1f56334b
Ari Lacenski <alacenski@gmail.com>
2012-10-16 10:02:54 -0700

    Pulling web form support out

--- server.py
+++ server.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, request
-from flask.ext.sendmail import Mail, Message
+from flask import Flask, render_template
 
 from contact_form import ContactForm
 
 app = Flask(__name__)
-mail = Mail(app)
 
 @app.route('/')
 def index():
@@ -15,25 +13,15 @@ def index():
 def projects():
 	return display('projects')
 	
-@app.route('/contact/', methods=['GET', 'POST'])
+@app.route('/contact/')
 def contact():
-	form = ContactForm(request.form)
-	if request.method == 'POST':
-		if form.validate():
-			pass
-		'''
-			msg = Message("Subject", sender=(request.form.sender, request.form.email_address))
-			msg.body = request.form.message
-			# Send email
-			mail.send(msg)
-		'''
-					
+	recipient = "alacenski@gmail.com"
+				
 	return render_template('contact.html', form=form)
 	
 def display(pagename):
 	return render_template('%s.html' % pagename, current=pagename)
 		
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
+	app.run(host='0.0.0.0')
 	
