321e61611278c63988b957789a0f4fc11f00a5c3
Ari Lacenski <alacenski@gmail.com>
2012-10-16 07:26:13 -0700

    Adding all desired fields to contact form

--- contact_form.py
+++ contact_form.py
@@ -1,6 +1,7 @@
-from wtforms import Form, TextField, TextAreaField, validators
+from wtforms import Form, TextField, TextAreaField, SubmitField, validators
 
 class ContactForm(Form):
 	sender = TextField('Name', [validators.Required])
 	email_address = TextField('Email address', [validators.Required, validators.Email])
 	message = TextAreaField('Message', [validators.Required])
+	submit = SubmitField('Send')
