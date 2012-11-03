0ffceb65e30813a7748c5c48572bf33c74a46ac1
root <root@diatom.arilacenski.com>
2012-11-03 01:49:16 +0000

    Renaming server.py to make it easier to run with gunicorn.

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return display('about')
+	
+@app.route('/projects/')
+def projects():
+	return display('projects')
+	
+def display(pagename):
+	return render_template('%s.html' % pagename, current=pagename)
+		
+if __name__ == '__main__':
+	app.run()
+	
