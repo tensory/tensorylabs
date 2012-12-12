b38144b50b1b03b5908d50bf205ddc2b485d2689
Ari Lacenski <alacenski@gmail.com>
2012-12-11 21:22:19 -0800

    Added in Scales project

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
-from flask import Flask, render_template
+from flask import Flask, make_response, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return display('about')
 	
+@app.route('/projects/scales')
+def scales():
+	return render_template('scales/index.html')
+
 @app.route('/projects/')
 def projects():
 	return display('projects')
-	
+
 def display(pagename):
 	return render_template('%s.html' % pagename, current=pagename)
-		
+
 if __name__ == '__main__':
 	app.run()
 	
